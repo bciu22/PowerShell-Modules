28b8cd5657566afc5f04173dcf338efeff5d8553
Charles <crossan007@gmail.com>
2016-05-27 08:47:18 -0400

    Dynamically determine IIS Logs

--- IIS/DeleteStaleIISLogs.ps1
+++ IIS/DeleteStaleIISLogs.ps1
@@ -22,8 +22,6 @@
 [CmdletBinding()]
 Param(
   [Parameter()]
-  $IISLogTarget = "C:\inetpub\logs\LogFiles\",
-  [Parameter()]
   $Days = "7",
   [Parameter()]
   $Extension = "*.log",
@@ -55,33 +53,41 @@ $mailBody = "$cn" + " $Now " + "`r`n`r`n"
 # Calculate stale date
 $LastWrite = $Now.AddDays(-$Days)
 
-#Get a list of files in the directory that meet the requirements
-$Files = Get-Childitem $IISLogTarget -Include $Extension -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
+Import-Module WebAdministration
+
+foreach($WebSite in $(get-website))
+{
+    $IISLogTarget="$($Website.logFile.directory)\w3svc$($website.id)".replace("%SystemDrive%",$env:SystemDrive)
+
+    #Get a list of files in the directory that meet the requirements
+    $Files = Get-Childitem $IISLogTarget -Include $Extension -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
 
 $LogLine = @"
 Starting Log Cleanup run.
+Website Name: $($WebSite.name)
 Hostname: $cn
 IISLogTarget: $IISLogTarget
 Days: $Days
 LastWrite: $LastWrite
 "@
-Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 100 -Message $LogLine
 
-foreach ($File in $Files) 
-{
-  Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 101 -Message "Deleting stale files in " + $IISLogTarget
-  if ($File -ne $NULL)
-  {
-    Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 102 -Message "Deleting File $File"
-    Remove-Item $File.FullName | Out-Null
-  }
-  else
-  {
-    Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 103 -Message  "No more files to delete!"
-  }
+    Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 100 -Message $LogLine
+
+    foreach ($File in $Files) 
+    {
+      Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 101 -Message "Deleting stale files in $IISLogTarget"
+      if ($File -ne $NULL)
+      {
+        Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 102 -Message "Deleting File $File"
+        Remove-Item $File.FullName | Out-Null
+      }
+      else
+      {
+        Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 103 -Message  "No more files to delete!"
+      }
+    }
+    Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 104 -Message "Completed execution for $($WebSite.name)"
 }
-Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 104 -Message "Completed execution."
-
 
 #Get the Events from the Error Log
 $Events = Get-EventLog -LogName "Application" -Source $LogName -After $(Get-Date -Hour 0 -Minute 0 -Second 0).AddDays(-1)
