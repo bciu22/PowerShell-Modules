78404075ef5b8878e8ab91bba5b93240f8774024
Charles <crossan007@gmail.com>
2016-10-19 08:51:42 -0400

    Add License Replacement Script

--- Office 365/Replace-UserLicense.ps1
+++ Office 365/Replace-UserLicense.ps1
@@ -1,3 +1,42 @@
+<#
+  .SYNOPSIS
+    This script will replace O365 licenses
+  
+  .PARAMETER Commit
+    False by default.  Setting this flag causes the script to make changes against the O365 environment.  Leaving this flag unset will cause the script to run in "read-only" mode, and no changes will be made.
+  
+  .PARAMETER OldLicense
+    Specify the AccountSkuId to be replaced     
+  .PARAMETER NewLicense
+    Specify the enw AccountSkuId
+  
+  .PARAMETER DisabledPlans
+    Specify an array of DisabledPlans
+    
+  .PARAMETER UserLimit
+    If set to 0, operate on all users.  Else, limit O365 users to this value.
+  
+  .LINK
+    https://support.office.com/en-us/article/Assign-or-unassign-licenses-for-Office-365-for-business-997596b5-4173-4627-b915-36abac6786dc
+  
+  .NOTES
+    Authors: Charles Crossan
+
+#>
+[CmdletBinding()]
+Param(
+  [Parameter()]
+  [Switch]$Commit=$False,
+  [Parameter()]
+  $OldLicense = "bucksiu:STANDARDWOFFPACK_FACULTY",
+  [Parameter()]
+  $NewLicense = "bucksiu:STANDARDWOFFPACK_IW_FACULTY",
+  [Parameter()]
+  $DisabledPlans = @("EXCHANGE_S_STANDARD"),
+  [Parameter()]
+  $UserLimit=0
+)
+
 $ExecutionStartTime = Get-Date
 $LogName = "O365 License Replacement Script"
 #Setup Logging
@@ -46,9 +85,6 @@ catch
 }
 
 
-$OldLicense = "bucksiu:STANDARDWOFFPACK_FACULTY"
-$NewLicense = "bucksiu:STANDARDWOFFPACK_IW_FACULTY"
-
 $NewLicensePlan = New-MsolLicenseOptions -AccountSkuId $NewLicense -DisabledPlans $DisabledPlans
 
 Function Has-UserLicense
@@ -66,12 +102,29 @@ Function Has-UserLicense
     }
 }
 
+if($UserLimit -eq 0)
+{
+  $Users = Get-MSOLUser -All 
+}
+else {
+   $Users = Get-MSOLUser -MaxResults $UserLimit
+}
+
 $UsersWithOldLicense = $users | ?{$(Has-UserLicense -user $_ -AccountSkuIU $OldLicense)}
+Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 127 -Message "Found $($UsersWithOldLicense.count) users with old license $($OldLicense)"
+
 
 Foreach ( $User in $UsersWithOldLicense )
 {
-    Write-Host "Updating User License for $($User.UserPrincipalName) from $OldLicense to $NewLicense"
-    $User | Set-MsolUserLicense -AddLicenses $NewLicense -LicenseOptions $LicenseOptions  -RemoveLicenses $OldLicense -ErrorAction "Stop"
+    
+    if ($Commit)
+    {
+      Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 128 -Message  "Updating User License for $($User.UserPrincipalName) from $OldLicense to $NewLicense"
+      $User | Set-MsolUserLicense -AddLicenses $NewLicense -LicenseOptions $NewLicensePlan  -RemoveLicenses $OldLicense -ErrorAction "Stop"
+    }
+    else {
+      Write-EventLog -LogName "Application" -Source $LogName -EntryType Information -EventID 129 -Message  "WhatIf: Updating User License for $($User.UserPrincipalName) from $OldLicense to $NewLicense"
+    }    
 
 }
 
